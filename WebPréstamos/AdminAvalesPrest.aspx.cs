013a83a6b68688e45d1b9b1a8e963313bd354cc3
jvisoso8 <jvisoso8@hotmail.com>
2018-10-23 09:47:29 -0500

    Add files via upload

---
+++ WebPréstamos/AdminAvalesPrest.aspx.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AdminAvalesPrest : System.Web.UI.Page {
+  //Atributos de la clase.
+  private DataSet DSPréstamos = new DataSet();
+  private DataSet DSAvales= new DataSet(), DSAvalan= new DataSet();
+  private DataRow fila;
+  private GestorBD.GestorBD GestorBD;       //Para manejar la BD.
+  private Comunes objComún = new Comunes();     //Para manejar las rutinas de uso común.
+  private String cadSql, rfc;
+  private int idAval;
+  private const int OK = 1;
+
+
+  //Acciones iniciales.
+  protected void Page_Load(object sender, EventArgs e) {
+    cargaGrid();
+  }
+
+  //Carga el grid con el contenido de la tabla Avalan.
+  public void cargaGrid() {
+
+    GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
+    cadSql = "select * from Avalan";
+    GestorBD.consBD(cadSql, DSAvalan, "Avalan");
+    GrdAvalan.DataSource = DSAvalan.Tables["Avalan"];
+    GrdAvalan.DataBind();
+  }
+
+  //Activa los controles que corresponda.
+  public void activaControles() {
+    BtnAlta.Enabled = false; BtnBaja.Enabled = false; BtnCambia.Enabled = false;
+    DdlPréstamos.Visible = true; DdlAvales.Visible = true;
+    TxtMonto.Visible = true; BtnEjecuta.Visible = true;
+    Label2.Visible = true; Label3.Visible = true; Label4.Visible = true;
+  }
+
+  //Desactiva los controles que corresponda.
+  public void desactivaControles() {
+    BtnAlta.Enabled = true; BtnBaja.Enabled = true; BtnCambia.Enabled = true;
+    DdlPréstamos.Visible = false;
+    DdlAvales.Visible = false; DdlAvales.Enabled = false;
+    TxtMonto.Visible = false; TxtMonto.Enabled = false;
+    BtnEjecuta.Visible = false;
+    Label2.Visible = false; Label3.Visible = false; Label4.Visible = false;
+  }
+
+  //========================================================================
+  //Parte 1a: acciones relacionadas con el alta de avales-préstamos.
+  protected void BtnAlta_Click(object sender, EventArgs e) {
+
+    //Recupera objetos de Session.
+    GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
+    rfc = Session["rfc"].ToString();
+
+    //Muestra/deshabilita controles asociados al alta.
+    activaControles();
+
+    //Carga los folios de los préstamos del cliente en DdlPréstamos.
+    cadSql = "select * from Préstamos where Rfc='" + rfc + "'";
+    GestorBD.consBD(cadSql, DSPréstamos, "Préstamos");
+    objComún.cargaDDL(DdlPréstamos, DSPréstamos, "Préstamos", "Folio");
+
+    //Carga el nombre de todos los avales en DdlAvales.
+    cadSql = "select * from Avales";
+    GestorBD.consBD(cadSql, DSAvales, "Avales");
+    objComún.cargaDDL(DdlAvales, DSAvales, "Avales", "IdAval");
+
+    //Indica la operación actual.
+    LblMensaje.Text = "Operación: Alta";
+    Session["Operación"] = "Alta";
+  }
+
+  //Habilita el DDL de Avales.
+  protected void DdlPréstamos_SelectedIndexChanged(object sender, EventArgs e) {
+    DdlAvales.Enabled = true;
+  }
+
+  //Habilita el monto y el botón de ejecutar.
+  protected void DdlAvales_SelectedIndexChanged(object sender, EventArgs e) {
+
+    TxtMonto.Enabled = true;
+    BtnEjecuta.Enabled = true;
+  }
+
+  //Termina de hacer el alta en la tabla: Avalan.
+  public void alta() {
+
+    //Recupera a GestorBD.
+    GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
+
+    idAval = Convert.ToInt16(DdlAvales.Text);   //Recupera la clave del aval.
+
+    //Si el monto es >= 50,000, hace el alta.
+    if (Convert.ToInt32(TxtMonto.Text) >= 50000) {
+      //Inserta en la tabla: Avalan.
+      cadSql = "insert into Avalan values(" + DdlPréstamos.Text +
+        "," + idAval + "," + TxtMonto.Text + ")";
+      if (GestorBD.altaBD(cadSql) == OK) {
+        LblMensaje.Text = "Inserción correcta";
+        cargaGrid();
+
+        //Oculta/habilita controles.
+        desactivaControles();
+      }
+      else
+        LblMensaje.Text = "No se hizo la inserción";
+    }
+    else {
+      LblMensaje.Text = "Dar un monto >= 50,000";
+      TxtMonto.Focus();
+    }
+  }
+
+  //Ejecuta la operación que corresponda.
+  protected void BtnEjecuta_Click(object sender, EventArgs e) {
+    string oper;
+
+    oper = Session["Operación"].ToString();
+    switch (oper) {
+      case "Alta":
+        alta();
+        break;
+      case "Cambio":
+        //cambio();
+        break;
+    }
+  }
+
+  //Acciones iniciales de la baja.
+  protected void BtnBaja_Click(object sender, EventArgs e) {
+
+    //Habilita/deshabilita controles asociados a la baja.
+    GrdAvalan.Enabled = true;
+    BtnAlta.Enabled = false; BtnBaja.Enabled = false; BtnCambia.Enabled = false;
+
+    LblMensaje.Text = "Operación: baja";
+  }
+  
+  //Elimina a la tupla elegida en el grid cuando se da clic a dicha tupla.
+  protected void GrdAvalan_RowDeleting(object sender, GridViewDeleteEventArgs e) {
+    int índiceFila;       //Índice de la fila elegida en el grid.
+    int índiceColInicial=2; //´Columna inicial a usar en el grid.
+    GridViewRow filaGrid;   //Tipo de la fila elegida en el grid.
+
+    //Recupera el índice de la fila y la fila de datos elegida en el grid.
+    índiceFila = e.RowIndex;
+    filaGrid = GrdAvalan.Rows[índiceFila];
+
+    //Borra de la tabla Avalan la fila elegida en el grid.
+    cadSql = "delete from Avalan where Folio= " +
+      filaGrid.Cells[índiceColInicial].Text + " and IdAval= " +
+      filaGrid.Cells[índiceColInicial + 1].Text;
+
+    GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
+    if (GestorBD.bajaBD(cadSql) == OK) {
+      cargaGrid();      //Actualiza el contenido del grid.
+      GrdAvalan.Enabled = false;
+      desactivaControles();   //Habilita/deshabilita controles.
+      LblMensaje.Text = "Operación: en espera";
+    }
+    else
+      LblMensaje.Text = "No se pudo eliminar de Avalan";
+  }
+
+  //Ejercicio extra: ejemplo de uso de una fila seleccionada en el grid.
+  protected void GrdAvalan_SelectedIndexChanged(object sender, EventArgs e) {
+
+
+
+    Response.Write(filaGrid.Cells[1].Text);
+
+  }
+}
+
---
+++ WebPréstamos/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page {
+
+  //Variables de clase.
+  GestorBD.GestorBD GestorBD;
+  DataSet DsGeneral = new DataSet();
+  string cadSql;
+
+  //Acciones iniciales.
+  protected void Page_Load(object sender, EventArgs e) {
+
+    //NOTA: La propiedad IsPostBack, de la página, tiene un valor False la 1a.
+    //vez que se carga la página; tiene True, en las veces subsecuentes.
+    if (!IsPostBack) {
+      GestorBD = new GestorBD.GestorBD("SQLNCLI11", "localhost",
+        "sa", "sqladmin", "Préstamos");
+      Session["GestorBD"] = GestorBD;
+    }
+  }
+
+  //Verifica que el usuario exista.
+  protected void Login1_Authenticate(object sender, AuthenticateEventArgs e) {
+
+    GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
+    //Verifica en la BD si hay coincidencia de Rfc y Contraseña.
+    cadSql = "select * from Clientes where Rfc='" + Login1.UserName + "' and " +
+      "Contraseña= '" + Login1.Password + "'";
+    GestorBD.consBD(cadSql, DsGeneral, "Usuario");
+    if (DsGeneral.Tables["Usuario"].Rows.Count != 0) {
+      Session["rfc"] = Login1.UserName;         //Sí existe, pasa a la página
+      Server.Transfer("AdminAvalesPrest.aspx");   //de lista de préstamos.
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
